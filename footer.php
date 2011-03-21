9b5aa271f4685dd0568e6f326de3eb08f6c3bb99
Michael Beckwith <tw2113@gmail.com>
2011-03-20 20:56:18 -0500

    fixed a couple missing items

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer role="contentinfo">
-			<p>&copy;<?php echo date('Y'); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			<p>&copy;<?php echo date('Y'); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 </footer>
 </div><!--End Container-->
 <?php 
