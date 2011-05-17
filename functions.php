04a5463eb91dc6f35bbc8e16a1e9b74be3639e33
Michael Beckwith <tw2113@gmail.com>
2011-05-16 21:16:46 -0500

    added browser detection filter to body_class

--- functions.php
+++ functions.php
@@ -251,6 +251,27 @@ function create_my_post_types() {
 	);
 }
 
+// browser detection via body_class
+add_filter('body_class','browser_body_class');
+
+function browser_body_class($classes) {
+    //WordPress global vars available.
+    global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+
+    if($is_lynx)       $classes[] = 'lynx';
+    elseif($is_gecko)  $classes[] = 'gecko';
+    elseif($is_opera)  $classes[] = 'opera';
+    elseif($is_NS4)    $classes[] = 'ns4';
+    elseif($is_safari) $classes[] = 'safari';
+    elseif($is_chrome) $classes[] = 'chrome';
+    elseif($is_IE)     $classes[] = 'ie';
+    else               $classes[] = 'unknown';
+
+    if($is_iphone) $classes[] = 'iphone';
+    
+    return $classes;
+}
+
 /*** Default Settings Cleanup and Adding Goodies **************************/
 
 /*
