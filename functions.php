514a1b8691e5fb2bef66273e6f046e99e4727d9a
Michael Beckwith <tw2113@gmail.com>
2011-06-06 01:04:11 -0500

    cleaned up some stuff, renamed some functions

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+/** This entire theme is based on TwentyTen from WordPress 3.1. Edited as I saw fit ****/
+
 /** Tell WordPress to run twentyten_setup() when the 'after_setup_theme' hook is run. */
 add_action( 'after_setup_theme', 'twentyten_setup' );
 
@@ -8,106 +10,30 @@ function twentyten_setup() {
     
     // Post Format support. You can also use the legacy "gallery" or "asides" (note the plural) categories. More info at http://codex.wordpress.org/Post_Formats
 	add_theme_support( 'post-formats', array( 'aside', 'audio', 'quote', 'link', 'image', 'video' ) );
-	// This theme uses post thumbnails
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' ); // This theme uses Featured Images
+	add_theme_support( 'automatic-feed-links' ); // Add default posts and comments RSS feed links to head
 
-	// Add default posts and comments RSS feed links to head
-	add_theme_support( 'automatic-feed-links' );
-
-	// Make theme available for translation
-	// Translations can be filed in the /languages/ directory
-	load_theme_textdomain( 'twentyten', TEMPLATEPATH . '/languages' );
-
-	$locale = get_locale();
-	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Navigation', 'twentyten' ),
-	) );
-
+	// This theme uses wp_nav_menu() in one location. Add more as needed
+	register_nav_menus( array( 'primary' => __( 'Primary Navigation', 'twentyten' ), ) );
 }
 endif;
 
-/**
- * Makes some changes to the <title> tag, by filtering the output of wp_title().
- *
- * If we have a site description and we're viewing the home page or a blog posts
- * page (when using a static front page), then we will add the site description.
- *
- * If we're viewing a search result, then we're going to recreate the title entirely.
- * We're going to add page numbers to all titles as well, to the middle of a search
- * result title and the end of all other titles.
- *
- * The site title also gets added to all titles.
- */
-function twentyten_filter_wp_title( $title, $separator ) {
-	// Don't affect wp_title() calls in feeds.
-	if ( is_feed() )
-		return $title;
-
-	// The $paged global variable contains the page number of a listing of posts.
-	// The $page global variable contains the page number of a single post that is paged.
-	// We'll display whichever one applies, if we're not looking at the first page.
-	global $paged, $page;
-
-	if ( is_search() ) {
-		// If we're a search, let's start over:
-		$title = sprintf( __( 'Search results for %s', 'twentyten' ), '"' . get_search_query() . '"' );
-		// Add a page number if we're on page 2 or more:
-		if ( $paged >= 2 )
-			$title .= " $separator " . sprintf( __( 'Page %s', 'twentyten' ), $paged );
-		// Add the site name to the end:
-		$title .= " $separator " . get_bloginfo( 'name', 'display' );
-		// We're done. Let's send the new title back to wp_title():
-		return $title;
-	}
-
-	// Otherwise, let's start by adding the site name to the end:
-	$title .= get_bloginfo( 'name', 'display' );
-
-	// If we have a site description and we're on the home/front page, add the description:
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title .= " $separator " . $site_description;
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		$title .= " $separator " . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-	// Return the new title to wp_title():
-	return $title;
-}
-add_filter( 'wp_title', 'twentyten_filter_wp_title', 10, 2 );
-
-/**
- * Sets the post excerpt length to 40 characters.
- */
-function twentyten_excerpt_length( $length ) {
-	return 40;
-}
+/** Sets the post excerpt length to 40 characters. */
+function twentyten_excerpt_length( $length ) { return 40; }
 add_filter( 'excerpt_length', 'twentyten_excerpt_length' );
 
-/**
- * Returns a "Continue Reading" link for excerpts
- */
+/** Returns a "Continue Reading" link for excerpts. */
 function twentyten_continue_reading_link() {
-	return ' <a href="'. get_permalink() . '">' . __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) . '</a>';
+	return '<a href="'. get_permalink() . '"> Continue reading <span class="meta-nav">&rarr;</span></a>';
 }
 
-/**
- * Replaces "[...]" (appended to automatically generated excerpts) with an ellipsis and twentyten_continue_reading_link().
- */
+/** Replaces "[...]" (appended to automatically generated excerpts) with an ellipsis and twentyten_continue_reading_link(). */
 function twentyten_auto_excerpt_more( $more ) {
 	return ' &hellip;' . twentyten_continue_reading_link();
 }
 add_filter( 'excerpt_more', 'twentyten_auto_excerpt_more' );
 
-/**
- * Adds a pretty "Continue Reading" link to custom post excerpts.
- */
+/** Adds a pretty "Continue Reading" link to custom post excerpts. */
 function twentyten_custom_excerpt_more( $output ) {
 	if ( has_excerpt() && ! is_attachment() ) {
 		$output .= twentyten_continue_reading_link();
@@ -116,18 +42,14 @@ function twentyten_custom_excerpt_more( $output ) {
 }
 add_filter( 'get_the_excerpt', 'twentyten_custom_excerpt_more' );
 
-/**
- * Remove inline styles printed when the gallery shortcode is used.
- */
+/** Remove inline styles printed when the gallery shortcode is used. */
 function twentyten_remove_gallery_css( $css ) {
 	return preg_replace( "#<style type='text/css'>(.*?)</style>#s", '', $css );
 }
 add_filter( 'gallery_style', 'twentyten_remove_gallery_css' );
 
 if ( ! function_exists( 'twentyten_comment' ) ) :
-/**
- * Template for comments and pingbacks.
- */
+/** Template for comments and pingbacks. */
 function twentyten_comment( $comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 	switch ( $comment->comment_type ) :
@@ -137,17 +59,17 @@ function twentyten_comment( $comment, $args, $depth ) {
 		<div id="comment-<?php comment_ID(); ?>">
 		<div class="comment-author vcard">
 			<?php echo get_avatar( $comment, 40 ); ?>
-			<?php printf( __( '%s <span class="says">says:</span>', 'twentyten' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+			<?php printf( '%s <span class="says">says:</span>', sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
 		</div><!-- .comment-author .vcard -->
 		<?php if ( $comment->comment_approved == '0' ) : ?>
-			<em><?php _e( 'Your comment is awaiting moderation.', 'twentyten' ); ?></em>
+			<em><?php echo 'Your comment is awaiting moderation.'; ?></em>
 			<br />
 		<?php endif; ?>
 
 		<div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
 			<?php
 				/* translators: 1: date, 2: time */
-				printf( __( '%1$s at %2$s', 'twentyten' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'twentyten' ), ' ' );
+				printf( '%1$s at %2$s', get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( '(Edit)', ' ' );
 			?>
 		</div><!-- .comment-meta .commentmetadata -->
 
@@ -159,23 +81,21 @@ function twentyten_comment( $comment, $args, $depth ) {
 	</div><!-- #comment-##  -->
 
 	<?php
-			break;
+		break;
 		case 'pingback'  :
 		case 'trackback' :
 	?>
 	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'twentyten' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'twentyten'), ' ' ); ?></p>
+		<p>Pingback: <?php comment_author_link(); ?><?php edit_comment_link( '(Edit)', ' ' ); ?></p>
 	<?php
-			break;
+		break;
 	endswitch;
 }
 endif;
 
-/**
- * Prints HTML with meta information for the current post—date/time and author.
- */
+/** Prints HTML with meta information for the current post—date/time and author. */
 function twentyten_posted_on() {
-	printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'twentyten' ),
+	printf( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s',
 	'meta-prep meta-prep-author', //%1$s
 		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date"><time datetime="%3$s">%4$s</time></span></a>',
 			get_permalink(), //inner %1$s
@@ -185,25 +105,23 @@ function twentyten_posted_on() {
 		),
 		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
 			get_author_posts_url( get_the_author_meta( 'ID' ) ),
-			sprintf( esc_attr__( 'View all posts by %s', 'twentyten' ), get_the_author() ),
+			sprintf( 'View all posts by %s', get_the_author() ),
 			get_the_author()
 		) //%3$s
 	);
 }
 
 if ( ! function_exists( 'twentyten_posted_in' ) ) :
-/**
- * Prints HTML with meta information for the current post (category, tags and permalink).
- */
+/** Prints HTML with meta information for the current post (category, tags and permalink). */
 function twentyten_posted_in() {
 	// Retrieves tag list of current post, separated by commas.
 	$tag_list = get_the_tag_list( '', ', ' );
 	if ( $tag_list ) {
-		$posted_in = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyten' );
+		$posted_in = 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
 	} elseif ( is_object_in_taxonomy( get_post_type(), 'category' ) ) {
-		$posted_in = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyten' );
+		$posted_in = 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
 	} else {
-		$posted_in = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyten' );
+		$posted_in = 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
 	}
 	// Prints the string, replacing the placeholders.
 	printf(
@@ -217,10 +135,10 @@ function twentyten_posted_in() {
 endif;
 
 /*** Custom Post Types **********************************************/
-//add_action( 'init', 'create_my_post_types' );
+//add_action( 'init', 'wpst_create_my_post_types' );
 
 //Gallery is just an example. Change as needed.
-function create_my_post_types() {
+function wpst_create_my_post_types() {
 	register_post_type( 'GALLERY',
 		array(
 			'labels' => array(
@@ -251,9 +169,9 @@ function create_my_post_types() {
 }
 
 // browser detection via body_class
-add_filter('body_class','browser_body_class');
+add_filter('body_class','wpst_browser_body_class');
 
-function browser_body_class($classes) {
+function wpst_browser_body_class($classes) {
     //WordPress global vars available.
     global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
 
@@ -271,11 +189,12 @@ function browser_body_class($classes) {
     return $classes;
 }
 // Customize footer text
-function remove_footer_admin () {
+function wpst_remove_footer_admin () {
     //echo "Your own text";
 } 
  
-add_filter('admin_footer_text', 'remove_footer_admin');
+//add_filter('admin_footer_text', 'wpst_remove_footer_admin');
+
 /*** Default Settings Cleanup and Adding Goodies **************************/
 
 /*
@@ -288,26 +207,24 @@ remove_action( 'wp_head', 'feed_links', 2 );
 remove_action('wp_head', 'wp_generator');
 
 /* adds the favicon/appleicon to the wp_head() call*/
-function blog_favicon() {
-echo '<link rel="shortcut icon" href="'.get_bloginfo('url').'/favicon.ico" />';
-}
-add_action('wp_head', 'blog_favicon');
-function apple_icon() {
-echo '<link rel="apple-touch-icon" href="'.get_bloginfo('url').'/apple-touch-icon.png" />';
-}
-add_action('wp_head', 'apple_icon');
+function wpst_blog_favicon() { echo '<link rel="shortcut icon" href="'.get_bloginfo('url').'/favicon.ico" />'; }
+add_action('wp_head', 'wpst_blog_favicon');
+
+function wpst_apple_icon() { echo '<link rel="apple-touch-icon" href="'.get_bloginfo('url').'/apple-touch-icon.png" />'; }
+add_action('wp_head', 'wpst_apple_icon');
 
 //Disable EditURI and WLWManifest
-add_action('init', 'remheadlink');
-function remheadlink() {
+function wpst_remheadlink() {
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
 }
+add_action('init', 'wpst_remheadlink');
+
 //removes Admin bar
 wp_deregister_script('admin-bar');
 wp_deregister_style('admin-bar');
 remove_action('wp_footer','wp_admin_bar_render',1000);
 
-// Includes the widgets.php file that defines all widget based functions. Done to clean up this file
-require_once( TEMPLATEPATH . '/widgets.php' );
+// Includes the widgets.php file that defines all widget based functions. Done to clean up this file Uncomment to use.
+//require_once( TEMPLATEPATH . '/widgets.php' );
 ?>
