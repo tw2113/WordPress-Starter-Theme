656eed1fc8c573a3eb07e1c1e633aaaa9c4f97bc
Michael Beckwith <tw2113@gmail.com>
2011-06-27 18:47:23 -0500

    added konami jquery snippet

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer role="contentinfo">
+<footer id="colophon" role="contentinfo">
 			<small>&copy;<?php echo date('Y'); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></small>
 </footer>
 </div><!--End Container-->
