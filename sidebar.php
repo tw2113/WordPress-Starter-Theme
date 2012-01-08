146b0b20a0f751f4c602137f9f3bf9a699a506a6
Michael Beckwith <tw2113@gmail.com>
2012-01-07 18:08:37 -0600

    missed ul tags in the sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,7 @@
 <aside role="complementary">
+<ul>
 <?php if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
 	<p>Sidebar stuff goes here </p>
 <?php endif; // end primary widget area ?>
-<?php
-    // Enter sidebar stuff here
-?>
+</ul>
 </aside>
