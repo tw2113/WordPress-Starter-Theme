126c392cfc84950dbd967fd6f43fdc428685b9d8
Michael Beckwith <tw2113@gmail.com>
2012-01-08 20:44:13 -0600

    combined loop-page and page.php

--- page.php
+++ page.php
@@ -1,8 +1,18 @@
 <?php get_header(); ?>
 <div class="content" role="main">
-    <?php
-		get_template_part( 'loop', 'page' );
-	?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header>
+		<?php if ( is_front_page() ) { ?>
+		<h2><?php the_title(); ?></h2>
+		<?php } else { ?>
+		<h1><?php the_title(); ?></h1>
+		<?php } ?>
+		</header><!--End Header_page-->
+
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
+		<?php edit_post_link( 'Edit', '', '' ); ?>
+	</article>
 <?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
