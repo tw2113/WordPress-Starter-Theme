a8dab1c6d5204622c77514ca3c360b4707bce3e6
Michael Beckwith <tw2113@gmail.com>
2012-01-12 00:05:27 -0600

    Added a remove_action for Adjacent posts in wp_head

--- functions.php
+++ functions.php
@@ -181,6 +181,7 @@ function wpst_headcleanup() {
 	remove_action('wp_head', 'rsd_link');
 	remove_action('wp_head', 'wlwmanifest_link');
 	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 	}
 add_action('init', 'wpst_headcleanup');
 
