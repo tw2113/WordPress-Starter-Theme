bc428fa71b98b343a9601a02c260c43d8a5a98bc
Michael Beckwith <tw2113@gmail.com>
2012-01-28 18:06:45 -0600

    re-added the loop php to some files that missed it

--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <div class="content" role="main">
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 	<article role="article" <?php post_class(); ?>>
 			<header>
 			<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
@@ -10,6 +11,7 @@
 			<?php the_content(); ?>
 
 		</article>
+		<?php endwhile; endif; ?>
 <?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
