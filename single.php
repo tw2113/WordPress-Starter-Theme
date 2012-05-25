7b5e63146fd55ea28edb386768a841e17067999e
Michael Beckwith <tw2113@gmail.com>
2012-05-25 01:13:05 -0500

    removed author description from single.php Never used it

--- single.php
+++ single.php
@@ -16,15 +16,6 @@
 				    <footer>
 				    <?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
 
-				    <?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
-					<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'wpst_author_bio_avatar_size', 60 ) ); ?>
-					<h2><?php printf( 'About %s', get_the_author() ); ?></h2>
-					<?php the_author_meta( 'description' ); ?>
-					<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-					<?php printf( 'View all posts by %s &rarr;', get_the_author() ); ?>
-					</a>
-					<?php endif; ?>
-
 					<?php wpst_posted_in(); ?>
 					<?php edit_post_link( 'Edit', '', '' ); ?>
 					<?php previous_post_link( '%link', '' . _x( '&larr;', 'Previous post link', 'twentyten' ) . ' %title' ); ?>
