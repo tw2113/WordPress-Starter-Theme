d7e9c4852b1fe51cdf7f3f5d9295ccd112eba5c6
Michael Beckwith <tw2113@gmail.com>
2012-08-03 02:07:58 -0500

    added mail.php for future form handling.

---
+++ inc/mail.php
@@ -0,0 +1,9 @@
+<?php /*$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
