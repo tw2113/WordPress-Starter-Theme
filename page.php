d42d542634f8aeccab1f637bd8172f663b553a6e
Michael Beckwith <tw2113@gmail.com>
2012-08-22 01:43:19 -0500

    removed conditional heading tags for page.php

--- page.php
+++ page.php
@@ -3,11 +3,7 @@
 	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header>
-		<?php if ( is_front_page() ) { ?>
 		<h2><?php the_title(); ?></h2>
-		<?php } else { ?>
-		<h1><?php the_title(); ?></h1>
-		<?php } ?>
 		</header><!--End Header_page-->
 
 		<?php the_content(); ?>
