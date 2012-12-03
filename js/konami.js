82a733d78c2e502645c0e0fecf16f4640f08f911
Michael Beckwith <tw2113@gmail.com>
2012-12-03 15:46:14 -0600

    rewrote js for konami stuff

--- js/konami.js
+++ js/konami.js
@@ -1,12 +1,42 @@
-/*
-An easy way to add some funny and entertaining stuff to your website. The Keys to enter for a Konami Code are : arrow up, arrow up, arrow down, arrow down, arrow left, arrow right, arrow left, arrow right, b, a ... and that's it !
-Borrowed from: http://forrst.com/posts/Easy_way_to_put_Konami_Code_on_your_website_with-zaK#comment-land
-*/
-var keys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+/*!
+ * SimpleKonami
+ * Copyright: WTFPL
+ * Version: 1
+ * Requires: jQuery v1.3.2 or later
+ */
+
+//Set up our array of needed keys, and variables.
+neededkeys = [38,38,40,40,37,39,37,39,66,65], started = false, count = 0;
 $(document).keydown(function(e){
-	keys.push( e.keyCode );
-	if ( keys.toString().indexOf( konami ) >= 0 ){
-		$(document).unbind('keydown',arguments.callee);
-			//DO STUFF
+	key = e.keyCode;
+	//Set start to true only if having pressed the first key in the konami sequence.
+	if(!started){
+		if(key == 38){
+			started = true;
+		}
+	}
+	//If we've started, pay attention to key presses, looking for right sequence.
+	if(started){
+		if(neededkeys[count] == key){
+			//We're good so far.
+			count++;
+		} else {
+			//Oops, not the right sequence, lets restart from the top.
+			reset();
+		}
+		if(count == 10){
+			//We made it! Put code here to do what you want when successfully execute konami sequence
+
+			//Reset the conditions so that someone can do it all again.
+			reset();
+		}
+	} else {
+	//Oops.
+		reset();
 	}
 });
+//Function used to reset us back to starting point.
+function reset() {
+	started = false; count = 0;
+	return;
+}
